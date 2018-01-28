16a59d0067965c4d106eb10fa8a2fe87b9012462
Jimson Vedua <jimson@jimsons-macbook-pro.local>
2018-01-28 15:20:26 -0500

    Initial Commit

---
+++ Concentrations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentrations
+//
+//  Created by Jimson Vedua on 1/28/18.
+//  Copyright © 2018 Jimson Vedua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
