4bceb6c7e76fc218155f3925cc9c090953cb0470
Jimson Vedua <jimson@Jimsons-MacBook-Pro.local>
2018-01-28 22:04:41 -0500

    first commit

--- Concentrations/ViewController.swift
+++ Concentrations/ViewController.swift
@@ -10,15 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
+ 
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var emojiChoices = ["👻","🎃","👻","🎃"]
+
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender){
+                flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }else{
+            print("chosen card was not in cardButtons")
+        }
     }
+    
+//    @IBAction func touchSecondCard(_ sender: UIButton) {
+//        flipCount += 1
+//        flipCard(withEmoji: "🎃", on: sender)
+//    }
+//
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        print("flipCard(withEmoji: \(emoji))")
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.6300166943, blue: 0.07593753069, alpha: 1)
+        }else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
 
 
 }
